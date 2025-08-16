bea7a5328baf133831ac9538f01f7f8db4b98b3b
Lock <81725500+dagemsyunoh@users.noreply.github.com>
2025-08-16 16:00:35 +0800

    fixed bug in item size comparator failing because of space
    Signed-off-by: Lock <81725500+dagemsyunoh@users.noreply.github.com>

--- app/src/main/java/com/lock/stockit/Helpers/SizeComparator.java
+++ app/src/main/java/com/lock/stockit/Helpers/SizeComparator.java
@@ -7,6 +7,8 @@ public class SizeComparator implements Comparator<String> {
     @Override
     public int compare(String o1, String o2) {
         if (o1.contains("x") && o2.contains("x")) {
+            o1 = o1.replace(" ", "");
+            o2 = o2.replace(" ", "");
             String[] d1 = o1.split("x");
             String[] d2 = o2.split("x");
             for (int i = 0; i < d1.length; i++) {
